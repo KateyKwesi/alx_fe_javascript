c75fee7db04c1cfdcbb70f56de0cc967eafab712
KateyKwesi <kateykwesi001@gmail.com>
2025-10-18 09:58:34 +0000

    initial commit

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,95 +1,64 @@
 const displayQuote = document.getElementById(`quoteDisplay`);
 const displayQuoteBtn = document.getElementById(`newQuote`);
-const quote = document.getElementById(`newQuoteText`);
-const category = document.getElementById(`newQuoteCategory`);
+const quoteValue = document.getElementById(`newQuoteText`);
+const categoryValue = document.getElementById(`newQuoteCategory`);
 const categoryDisplay = document.getElementById("categoryDisplay");
 const textDisplay = document.getElementById("textDisplay");
 const authorDisplay = document.getElementById("authorDisplay");
 
-const quotesArr = [
-  {
-    category: "Philosophy",
-    text: "I think, therefore I am.",
-    author: "René Descartes",
-  },
-  {
-    category: "Politics/History",
-    text: "I may disagree with what you say, but I will defend to death your right to say it.",
-    author: "Voltaire",
-  },
-  {
-    category: "Self-Improvement",
-    text: "It is never too late to be what you might have been.",
-    author: "George Eliot",
-  },
-  {
-    category: "Life",
-    text: "Life is like riding a bicycle. To keep your balance, you must keep moving.",
-    author: "Albert Einstein",
-  },
-  {
-    category: "Motivation",
-    text: "You miss 100% of the shots you don't take.",
-    author: "Wayne Gretzky",
-  },
-  {
-    category: "Deep/Philosophical",
-    text: "The only thing we have to fear is fear itself.",
-    author: "Franklin D. Roosevelt",
-  },
-  {
-    category: "Friendship",
-    text: "If men were perfectly virtuous, they wouldn't have friends.",
-    author: "Friedrich Nietzsche",
-  },
-  {
-    category: "Success",
-    text: "Do the best you can. No one can do more than that.",
-    author: "John Wooden",
-  },
-  {
-    category: "Character",
-    text: "To thine own self, be true.",
-    author: "William Shakespeare",
-  },
-  {
-    category: "Human Nature",
-    text: "The truth will set you free.",
-    author: "Jesus Christ",
-  },
-];
+const quotesArr = [];
 
-function getRandom() {
+const fetchQuote = async () => {
+  const response = await fetch(
+    `https://api.api-ninjas.com/v1/quotes
+`,
+    {
+      method: `GET`,
+      headers: { "X-Api-Key": "bg8PPHI0HV/Lkj2TvkyRDw==nKrDLr62blt9K3eE" },
+    }
+  );
+  const jsonData = await response.json();
+  return jsonData[0];
+};
+
+async function getRandom() {
+  const quote = await fetchQuote();
+  quotesArr.push(quote);
+
   const randomIndex = Math.floor(Math.random() * quotesArr.length);
   return quotesArr[randomIndex];
 }
 
-function showRandomQuote({ category, text, author }) {
-  categoryDisplay.innerHTML = `Category:  ${category}`;
-  textDisplay.innerHTML = `quote:  ${text}`;
-  //.textContent = `author : ${author}`;
+function appendToDom(category, quote) {
+  displayQuote.innerHTML = ``;
+  let categoryPara = document.createElement(`p`);
+  let quotePara = document.createElement(`p`);
+  categoryPara.innerHTML = `Category:  ${category}`;
+  quotePara.innerHTML = `quote:  ${quote}`;
+  displayQuote.appendChild(categoryPara);
+  displayQuote.appendChild(quotePara);
+}
+
+function showRandomQuote({ category, quote, author }) {
+  appendToDom(category, quote);
 }
 
 function createAddQuoteForm() {
-  let newQuote = quote.value;
-  let quoteCategory = category.value;
+  let quote = quoteValue.value;
+  let category = categoryValue.value;
 
   quotesArr.push({
-    category: `${quoteCategory}`,
-    text: `${newQuote}`,
+    category: `${category}`,
+    text: `${quote}`,
   });
 
-  categoryDisplay.textContent = `Category:  ${quoteCategory}`;
-  textDisplay.textContent = `quote:  ${newQuote}`;
-
-  //i did this just to pass the auto checker
-  document.createElement(``);
-  document.appendChild(``);
+  appendToDom(category, quote);
 }
 function addQuote() {
   createAddQuoteForm();
 }
 
-displayQuoteBtn.addEventListener("click", () => {
-  showRandomQuote(getRandom());
+displayQuoteBtn.addEventListener("click", async () => {
+  const waitRandomQuote = await getRandom();
+  showRandomQuote(waitRandomQuote);
 });
